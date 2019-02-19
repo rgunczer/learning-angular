ec93f12ce6b487af9581d1b77869cd78bb7f296a
rgunczer <rgunczer@gmail.com>
2019-02-19 23:09:44 +0100

    filter before subscribe

--- facadeApp/src/app/app.component.ts
+++ facadeApp/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
 import { CommonApiService } from './common-api.service';
+// import { Util } from './util';
+
+function getRndInteger(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -9,8 +17,19 @@ import { CommonApiService } from './common-api.service';
 export class AppComponent implements OnInit {
   title = 'facadeApp';
 
-  constructor(private commonApi: CommonApiService) {
+  subject = new Subject<number>();
 
+  constructor(private commonApi: CommonApiService) {
+    this.subject
+    .pipe(
+      tap((num: number) => console.log('got: ', num)),
+      filter((num: number) => num > 3)
+    )
+    .subscribe(
+      (num: number) => {
+        console.log('the number is:', num);
+      }
+    );
   }
 
   ngOnInit() {
@@ -23,4 +42,7 @@ export class AppComponent implements OnInit {
       );
   }
 
+  send() {
+    this.subject.next(getRndInteger(1, 9));
+  }
 }
