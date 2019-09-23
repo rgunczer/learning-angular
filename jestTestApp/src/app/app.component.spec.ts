e8a8dffa36f9f7a03d120d30a79cab0b10ce299c
rgunczer <rgunczer@gmail.com>
2019-09-23 22:52:08 +0200

    new jest test app

--- jestTestApp/src/app/app.component.spec.ts
+++ jestTestApp/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jestTest01'`, () => {
+  it(`should have as title 'jestTestApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('jestTest01');
+    expect(app.title).toEqual('jestTestApp');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to jestTest01!');
+    expect(compiled.querySelector('.content span').textContent).toContain('jestTestApp app is running!');
   });
 });
