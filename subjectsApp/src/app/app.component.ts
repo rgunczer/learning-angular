ebf745bc64e68587238ffe92cb597d3375fa010e
rgunczer <rgunczer@gmail.com>
2019-10-08 23:07:10 +0200

    rxjs subjects test app

---
+++ subjectsApp/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Subject, BehaviorSubject, ReplaySubject } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'subjectsApp';
+  subject: Subject<string>;
+  behaviorSubject: BehaviorSubject<string>;
+  replaySubject: ReplaySubject<string>;
+
+  ngOnInit() {
+    this.subject = new Subject();
+
+    setTimeout(() => {
+      this.subject.next('joel');
+    });
+
+    this.subject.subscribe((value: string) => {
+      console.log(`got: ${value}`);
+    });
+
+    this.behaviorSubject = new BehaviorSubject('first');
+
+    this.behaviorSubject.subscribe((value: string) => {
+      console.log(`behavior subject:  ${value}`);
+    });
+
+    this.replaySubject = new ReplaySubject(3);
+
+    this.replaySubject.subscribe((value: string) => {
+      console.log(`replay subject: ${value}`);
+    });
+
+    this.replaySubject.next('apple');
+    this.replaySubject.next('kiwi');
+    this.replaySubject.next('banana');
+  }
+
+  addNewSubscriberFor(type: string): void {
+    this.replaySubject.subscribe((value: string) => {
+      console.log(`new subscriber: ${value}`);
+    });
+  }
+
+  emitNewValue(type: string, value: string): void {
+    this.replaySubject.next(value);
+  }
+
+}
