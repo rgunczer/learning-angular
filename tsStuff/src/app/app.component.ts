4a11f72145957170f292c213eee79c91716fedb7
rgunczer <rgunczer@gmail.com>
2019-10-10 23:02:18 +0200

    playing with keyof

---
+++ tsStuff/src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { Person } from './person.model';
+
+interface MsgboxTypes {
+  error: string;
+  ok: string;
+  yesno: string;
+  yesnocancel: string;
+  continueanyway: string;
+  gunsnroses: string
+}
+
+interface Todo {
+  id: number;
+  text: string;
+  due: Date;
+  age: number;
+}
+
+enum Colors {
+  Red = 'r',
+  Green = 'g',
+  Blue = 'b'
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent implements OnInit {
+  title = 'tsStuff';
+  boss: Person;
+
+  ngOnInit() {
+
+    type TodoKeys = keyof Todo; // "id" | "text" | "due"
+
+    let key: TodoKeys = 'age';
+
+    function get<O extends object, K extends keyof O>(o: O, k: K): O[K] {
+      return o[k];
+    }
+
+    function extractPropValue<T, K extends keyof T>(obj: T, key: K) {
+      return obj[key];
+    }
+
+    this.boss = new Person(1, 'aaa');
+
+
+    const cc = get(this.boss, 'id');
+
+    const propValue = extractPropValue(this.boss, 'name');
+    console.log(propValue);
+
+    this.messageBox('ok', 'apple is here');
+    this.messageBox('gunsnroses', 'hello')
+  }
+
+
+  messageBox(
+    type: keyof MsgboxTypes, // 'error' | 'ok' | 'yesno' | 'yesnocancel' | 'continueanyway',
+    title: string
+  ) {
+    console.log(type + ' ' + title);
+  }
+
+}
